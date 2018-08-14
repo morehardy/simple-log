08a7d81bb67f98fc620a28eab8c8ab3d3b8237a5
hepeng <morehardy@163.com>
2018-08-14 13:33:22 +0800

    feat: add img directive

--- src/directive.js
+++ src/directive.js
@@ -1,10 +1,21 @@
+import iconKeys from './icon'
+
 // clear
 export const clear = () => {
   return console.clear()
 }
 
 // spread
-export const spread = (spread = '-') => {
-  const str = 'spread'.padEnd(200, spread.toString())
+export const spread = (spread = '-', num = 200) => {
+  const str = 'spread'.padEnd(num + 6, spread.toString())
   return console.log(str)
+}
+
+// img
+export const img = key => {
+  // constructor
+  const creatImg = url => {
+    console.log('%c ', `padding:50px;background:url(${url}); background-repeat: no-repeat; background-size: contain; background-position: center;`)
+  }
+  iconKeys[key] ? creatImg(iconKeys[key]) : creatImg(key)
 }
---
+++ src/icon.js
@@ -0,0 +1,21 @@
+const apple = 'https://github.com/morehardy/simple-log/blob/master/images/apple.png?raw=true'
+const accessory = 'https://github.com/morehardy/simple-log/blob/master/images/accessory.png?raw=true'
+const cherry = 'https://github.com/morehardy/simple-log/blob/master/images/cherry.png?raw=true'
+const heart = 'https://github.com/morehardy/simple-log/blob/master/images/heart.png?raw=true'
+const pen = 'https://github.com/morehardy/simple-log/blob/master/images/pen.png?raw=true'
+const pitaya = 'https://github.com/morehardy/simple-log/blob/master/images/pitaya.png?raw=true'
+const tomato = 'https://github.com/morehardy/simple-log/blob/master/images/tomato.png?raw=true'
+const watermelon = 'https://github.com/morehardy/simple-log/blob/master/images/watermelon.png?raw=true'
+
+const iconKeys = {
+  apple,
+  accessory,
+  cherry,
+  heart,
+  pen,
+  pitaya,
+  tomato,
+  watermelon
+}
+
+export default iconKeys
